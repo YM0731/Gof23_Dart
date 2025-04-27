6b533dbbe87583dea6b8fc06e19170a8bd49171f
YM0731 <44638879+YM0731@users.noreply.github.com>
2025-04-27 14:50:09 +0900

    Adapterパターン

---
+++ Adapter/main.dart
@@ -0,0 +1,41 @@
+/// Target クライアントが使用するインターフェース
+/// Adapter Targetインターフェースを実装し、Adpteeを呼び出す
+/// Adaptee 変換されるインターフェースを持つ既存のクラス
+
+void main() {
+  OldMediaPlayer oldMediaPlayer = OldMediaPlayer();
+  MediaPlayer player = MediaPlayerAdpter(oldMediaPlayer);
+  player.play('song.mp3');
+}
+
+/// 既存のインターフェース（Adaptee）
+class OldMediaPlayer {
+  /// 音楽再生
+  void playOldMedia(String fileName) {
+    print(fileName);
+  }
+}
+
+/// 新しいインターフェース（Target）
+abstract class MediaPlayer {
+  void play(String fileName);
+}
+
+/// Adapterクラス
+class MediaPlayerAdpter implements MediaPlayer {
+  /// 既存のインターフェース
+  late OldMediaPlayer _oldMediaPlayer;
+
+  /// コンストラクタ
+  ///
+  /// [oldMediaPlayer] 既存のインターフェース
+  MediaPlayerAdpter(OldMediaPlayer oldMediaPlayer) {
+    this._oldMediaPlayer = oldMediaPlayer;
+  }
+
+  /// 音楽作成
+  @override
+  void play(String fileName) {
+    _oldMediaPlayer.playOldMedia(fileName);
+  }
+}
