9d138ecef795b3d913f2a9623f275f6269459c16
YM0731 <44638879+YM0731@users.noreply.github.com>
2025-04-28 08:30:34 +0900

    Factory Methodパターン

--- Factory Method/main.dart
+++ Factory Method/main.dart
@@ -1,34 +1,30 @@
-void main() {
-  PokemonFactory pikachuFactory = PikachuFactory();
-  Pokemon pikachu = pikachuFactory.createPokemon();
-  pikachu.getName();
+/// 使用：使用出来る
+/// 内容：インスタンスの生成をサブクラスに任せることで、オブジェクトの生成方法を柔軟に変更できる
+/// 使用方法：
+/// 　クラスのインスタンス化を動的に行う必要がある場合に役立つ。
+/// 　通常のインスタンス生成では、クライアントコードが具体的なクラス名に依存するため、
+/// 　クラス名の変更や拡張に柔軟に対応するのが難しくなるが、
+/// 　Factory Methodを使用することでクライアントコードがどのクラスを使用しているかを
+/// 　意識せずにインスタンス生成を行うことが出来る。
 
-  PokemonFactory rattataFactory = RattataFactory();
-  Pokemon rattata = rattataFactory.createPokemon();
+void main() {
+  PokemonFactory factory = PokemonFactory();
+  Pokemon pikachu = factory.createPokemon(PokemonType.pikachu);
+  Pokemon rattata = factory.createPokemon(PokemonType.rattata);
+  pikachu.getName();
   rattata.getName();
 }
 
 /// Factory基底クラス
-abstract class PokemonFactory {
+class PokemonFactory {
   /// ポケモン生成
-  Pokemon createPokemon();
-}
-
-/// ピカチュウFactoryクラス
-class PikachuFactory extends PokemonFactory {
-  /// ポケモン生成
-  @override
-  Pokemon createPokemon() {
-    return Pikachu();
-  }
-}
-
-/// コラッタFactoryクラス
-class RattataFactory extends PokemonFactory {
-  /// ポケモン生成
-  @override
-  Pokemon createPokemon() {
-    return Rattata();
+  Pokemon createPokemon(PokemonType pokemon) {
+    switch (pokemon) {
+      case PokemonType.pikachu:
+        return Pikachu();
+      case PokemonType.rattata:
+        return Rattata();
+    }
   }
 }
 
@@ -54,4 +50,12 @@ class Rattata extends Pokemon {
   void getName() {
     print('Rattata');
   }
+}
+
+enum PokemonType {
+  /// ピカチュウ
+  pikachu,
+
+  /// コラッタ
+  rattata,
 }
